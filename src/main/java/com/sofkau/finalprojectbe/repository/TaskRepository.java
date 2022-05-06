96b8f1999049fb4326b40378be3db06f42540a63
Jhonny Castro Clavijo <johnny.castro@misena.edu.co>
2022-05-06 02:02:07 -0500

    Project Service
    Task service was implemented

--- src/main/java/com/sofkau/finalprojectbe/repository/TaskRepository.java
+++ src/main/java/com/sofkau/finalprojectbe/repository/TaskRepository.java
@@ -1,9 +1,13 @@
 package com.sofkau.finalprojectbe.repository;
 
+import com.sofkau.finalprojectbe.entity.Category;
 import com.sofkau.finalprojectbe.entity.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
+    List<Task> findAllByCategory(Category category);
 }
--- src/main/java/com/sofkau/finalprojectbe/service/TaskService.java
+++ src/main/java/com/sofkau/finalprojectbe/service/TaskService.java
@@ -2,6 +2,9 @@ package com.sofkau.finalprojectbe.service;
 
 import com.sofkau.finalprojectbe.entity.Category;
 import com.sofkau.finalprojectbe.entity.Task;
+import com.sofkau.finalprojectbe.exceptions.CategoryNotFoundException;
+import com.sofkau.finalprojectbe.exceptions.TaskNotFoundException;
+import com.sofkau.finalprojectbe.repository.CategoryRepository;
 import com.sofkau.finalprojectbe.repository.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,24 +16,43 @@ public class TaskService implements ITaskService {
 
     @Autowired
     private TaskRepository taskRepository;
+    private CategoryRepository categoryRepository;
 
     @Override
     public List<Task> getAllTasksByCategory(Category category) {
-        return null;
+
+        Category categoryFilter = categoryRepository.findById(category.getId()).orElse(null);
+
+        if (categoryFilter == null) {
+            throw new CategoryNotFoundException("Category not found in the list of tasks filtered by category " + category.getId())
+        }
+
+        List<Task> tasks = taskRepository.findAllByCategory(category);
+
+        return tasks;
     }
 
     @Override
     public Task saveTask(Task task) {
-        return null;
+        Category categoryFilter = categoryRepository.findById(task.getCategory().getId()).orElse(null);
+
+        if (categoryFilter == null) {
+            throw new CategoryNotFoundException("Category not found while adding the task " + task.getId())
+        }
+        return taskRepository.save(task);
     }
 
     @Override
     public Task updateTask(Task task) {
-        return null;
+        return taskRepository.save(task);
     }
 
     @Override
     public void deleteTask(Long id) {
+        Task task = taskRepository.findById(id).orElse(null);
 
+        if (task == null) {
+            throw new TaskNotFoundException("Task not found while deleting it " + task.getId())
+        }
     }
 }
