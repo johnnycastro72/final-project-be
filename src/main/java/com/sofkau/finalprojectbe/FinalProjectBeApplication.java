4c094e17370b5d9d00b3dfd46777edd7e14821af
Jhonny Castro Clavijo <johnny.castro@misena.edu.co>
2022-05-07 23:44:00 -0500

    Documentation
    Documentation in progress

--- src/main/java/com/sofkau/finalprojectbe/FinalProjectBeApplication.java
+++ src/main/java/com/sofkau/finalprojectbe/FinalProjectBeApplication.java
@@ -5,6 +5,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+/**
+ * This is the main class.
+ * <p>
+ *
+ * @author Jhonny Castro johnny.castro@misena.edu.co
+ * @version 1.0.0 7/05/2022
+ * @since 1.0.0
+ *
+ * @params args <String[]>
+ */
 @SpringBootApplication
 public class FinalProjectBeApplication {
 
